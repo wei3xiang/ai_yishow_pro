dc7b9a0ee089c842e5b54491671e5edf7409dfae
tfb-jiangxingyu <jiangxingyu@qq.com>
2024-10-23 18:19:39 +0800

    初始化

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 
-import AiFranchisee from './aiFranchisee';
-
 const Navbar = () => {
-  const [activePage, setActivePage] = useState("aiFranchisee");
+  const [activePage, setActivePage] = useState("");
 
   const renderPage = () => {
     switch (activePage) {
-        case "aiFranchisee":
-            return <AiFranchisee />;
-        default:
-            return <AiFranchisee />;
+      default:
+        return <></>;
     }
   };
 
@@ -18,19 +14,7 @@ const Navbar = () => {
     <div>
       <div className="layui-side layui-bg-black">
         <div className="layui-side-scroll">
-          <ul className="layui-nav layui-nav-tree" lay-filter="test">
-                <li
-                className={`layui-nav-item ${
-                    activePage === "aiFranchisee" ? "layui-this" : ""
-                }`}
-                >
-                <a href="#" onClick={() => setActivePage("aiFranchisee")}>
-                  {"物流公司加盟信息表".endsWith("表")
-                  ? "物流公司加盟信息表".slice(0, -1) + "管理"
-                  : "物流公司加盟信息表"}
-                </a>
-                </li>
-          </ul>
+          <ul className="layui-nav layui-nav-tree" lay-filter="test"></ul>
         </div>
       </div>
       <div className="layui-header">
